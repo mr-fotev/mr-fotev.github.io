0f3e9d43885eaf414ecc25b5d9769c0c6d122d65
Mr. Fotev <itsofaraona@abv.bg>
2018-08-17 20:44:38 +0300

    New version

--- earley/earley.pl
+++ earley/earley.pl
@@ -20,37 +20,28 @@ complete([Y,_,[],_,Ty], [X,A,[Y|B],I,T], [X,AY,B,I,NewT]):- append(A, [Y], AY), 
 
 e(_, Nulls, _, _, _, [], Curr, Next, [Curr, Next, Nulls]).
 
-e([N,E,S,R], Nulls, [C|Cs], L, K, [[X,A,[C|B],I,T]|Xs], Curr, Next, Res):- scan([X,A,[C|B],I,T], NewNode),
-	addSet(Next, NewNode, NewNext), e([N,E,S,R], Nulls, [C|Cs], L, K, Xs, Curr, NewNext, Res).
-
-e([N,E,S,R], Nulls, [C|Cs], L, K, [[_,_,[D|_],_,_]|Xs], Curr, Next, Res):- member(D, E), D \= C,
-	e([N,E,S,R], Nulls, [C|Cs], L, K, Xs, Curr, Next, Res).
-
-e([N,E,S,R], Nulls, [], L, K, [[_,_,[C|_],_,_]|Xs], Curr, Next, Res):- member(C, E),
-	e([N,E,S,R], Nulls, [], L, K, Xs, Curr, Next, Res).
+e([N,E,S,R], Nulls, Cs, L, K, [[X,A,[D|B],I,T]|Xs], Curr, Next, Res):- member(D,E), Node = [X,A,[D|B],I,T],
+	(Cs = [D|_] -> (scan(Node, NewNode), addSet(Next, NewNode, NewNext)); NewNext = Next),
+	e([N,E,S,R], Nulls, Cs, L, K, Xs, Curr, NewNext, Res).
 
 e([N,E,S,R], Nulls, Cs, L, K, [[X,A,[Y|B],I,T]|_], Curr, Next, Res):- member([Y|Ys], R), Node = [X,A,[Y|B],I,T],
 	findall(NewNode, predict(Nulls, Ys, Node, K, NewNode), NewNodes),
 	bulkAddSet(Curr, NewNodes, NewCurr), append(_, [Node|NewXs], NewCurr),
 	e([N,E,S,R], Nulls, Cs, L, K, NewXs, NewCurr, Next, Res).
 
-e([N,E,S,R], Nulls, Cs, L, K, [[X,A,[],K,T]|_], Curr, Next, Res):- Node = [X,A,[],K,T],
-	findall(NewNode, (member(TempNode, Curr), complete(Node, TempNode, NewNode)), NewNodes),
-	bulkAddSet(Curr, NewNodes, NewCurr), append(_, [Node|NewXs], NewCurr),
-	addNulls(Nulls, [X,T], NewNulls), e([N,E,S,R], NewNulls, Cs, L, K, NewXs, NewCurr, Next, Res).	
-
-e([N,E,S,R], Nulls, Cs, L, K, [[X,A,[],I,T]|_], Curr, Next, Res):- nth0(I, L, LI), Node = [X,A,[],I,T],
+e([N,E,S,R], Nulls, Cs, L, K, [[X,A,[],I,T]|_], Curr, Next, Res):- Node = [X,A,[],I,T],
+	(I < K -> (nth0(I, L, LI), NewNulls = Nulls); (LI = Curr, addNulls(Nulls, [X,T], NewNulls))),
 	findall(NewNode, (member(TempNode, LI), complete(Node, TempNode, NewNode)), NewNodes),
 	bulkAddSet(Curr, NewNodes, NewCurr), append(_, [Node|NewXs], NewCurr),
-	e([N,E,S,R], Nulls, Cs, L, K, NewXs, NewCurr, Next, Res).
+	e([N,E,S,R], NewNulls, Cs, L, K, NewXs, NewCurr, Next, Res).
 
 % Main predicate
 
 earley([N,E,S,R], Nulls, [], L, K, Curr, Res):- e([N,E,S,R], Nulls, [], L, K, Curr, Curr, [], [Final, _, _]),
-	member([S, _, [], 0, Res], Final).
+	member([S,_,[],0,Res], Final).
 
 earley([N,E,S,R], Nulls, [C|Cs], L, K, Curr, Res):- e([N,E,S,R], Nulls, [C|Cs], L, K, Curr, Curr, [], [NewCurr, NewNext, NewNulls]), 
 	M is K + 1, append(L, [NewCurr], NewL), earley([N,E,S,R], NewNulls, Cs, NewL, M, NewNext, Res).
 
 earley([N,E,S,R], W, Res):- findall([X, [X,[]]], (member([X|Xs], R), member([], Xs)), Nulls),
-	earley([N,E,S,R], Nulls, W, [], 0, [[#, [], [S], 0, [[#]]]], Res).
+	earley([N,E,S,R], Nulls, W, [], 0, [[#,[],[S],0,[[#]]]], Res).
